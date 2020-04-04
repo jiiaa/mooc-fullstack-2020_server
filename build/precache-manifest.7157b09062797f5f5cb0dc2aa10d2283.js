ab4fdca44b71f36d798c4b4618c62870a2c05a39
jiiaa <jukkaa@live.com>
2020-04-04 17:46:18 +0300

    Adding static public folder.

---
+++ build/precache-manifest.7157b09062797f5f5cb0dc2aa10d2283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b1444448a18d4ab4e1e7d67b8ea356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96d24d3f562e05c6c80a",
+    "url": "/static/css/main.e9d11452.chunk.css"
+  },
+  {
+    "revision": "c574ff2531d61c703e37",
+    "url": "/static/js/2.16e154f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16e154f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d24d3f562e05c6c80a",
+    "url": "/static/js/main.0904d208.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
